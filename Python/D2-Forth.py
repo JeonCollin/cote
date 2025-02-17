3f7e5450f03c0e3bfe9101c38c91ba8e25577d30
Sangwoo Jeon <167081049+JeonCollin@users.noreply.github.com>
2025-02-17 20:40:49 +0900

    Update D2-Forth.py

--- Python/D2-Forth.py
+++ Python/D2-Forth.py
@@ -24,7 +24,45 @@ def pop():
     
 def calculator(mylist):
     
-    
+    L = len(mylist)
+
+    # 문자열을 순회하며 연산자/피연산자 확인
+    for i in range(L):
+
+        # print(stack)
+        # print(mylist[i])
+
+        # 숫자라면 push
+        if(mylist[i].isdigit() == True):
+            push(int(mylist[i]))
+
+        # 끝인 .에 도달했으면 끝
+        elif(mylist[i] == '.'):
+            return pop()
+
+        # 연산자면 pop
+        else:
+            num1 = pop()
+            num2 = pop()
+
+            # 숫자나 연산자가 남으며 에러
+            if(num1 == False or num2 == False):
+                return 'error'
+
+            # 나중에 나온 수가 앞으로 가야 함
+            # 연산 결과를 다시 push한다
+            if(mylist[i] == '+'):
+                push(num2 + num1)
+
+            elif(mylist[i] == '-'):
+                push(num2-num1)
+            
+            elif(mylist[i] == '*'):
+                push(num2*num1)
+            
+            elif(mylist[i] == '/'):
+                push(num2//num1)
+            
 
 
 T = int(input())
@@ -39,6 +77,7 @@ for t in range(1, T+1):
     stack = ['']*N
     top = -1
 
-    # 스택에 문자열 넣기
-    for i in range(N):
-        push(mylist[i])
+    # 결과 출력
+    print(f'#{t} {calculator(mylist)}')
+
+    
